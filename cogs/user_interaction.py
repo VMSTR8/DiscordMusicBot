9dc396fc0a4423afb291513a54791e6c38a9ed19
Maxim Vinokurov <vmstr8@yandex.ru>
2023-08-10 18:17:54 +0300

    Getting Started with the User Interaction feature

---
+++ cogs/user_interaction.py
@@ -0,0 +1,30 @@
+import discord
+from discord import app_commands, Interaction
+from discord.ext import commands
+
+
+class UserInteractionCog(commands.Cog):
+
+    def __init__(self, bot: commands.Bot) -> None:
+
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member) -> None:
+        pass
+
+    @app_commands.command(
+        name='grant_permission',
+        description='Отправить список из 5 вайфу с сайта shikimori.me для получения доступа в голосовые каналы'
+    )
+    @app_commands.describe(
+        role='Напиши название своей роли, которую я создам и присвою тебе',
+        shikimori_urls='Ссылки на 5 вайфу с сайта shikimori.me через запятую. Первой всегда идет твоя самая-самая!'
+        )
+    async def grant_permission(
+            self,
+            interaction: Interaction,
+            role: str,
+            shikimori_urls: str) -> None:
+        pass
+    
