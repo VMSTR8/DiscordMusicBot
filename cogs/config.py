6a2dea72aa1f2b86d86d0ae5018a9509da33576f
Maxim Vinokurov <vmstr8@yandex.ru>
2023-08-24 01:18:56 +0300

    config file for user_interaction

---
+++ cogs/config.py
@@ -0,0 +1,24 @@
+general_permissions = {
+    'view_channel': True,
+    'add_reactions': True,
+    'attach_files': True,
+    'external_emojis': True,
+    'external_stickers': True,
+    'send_messages': True,
+    'send_messages_in_threads': True,
+    'read_messages': True,
+    'read_message_history': True,
+    'use_application_commands': False,
+}
+
+voice_channel_permissions = {
+    'connect': True,
+    'speak': True,
+    'stream': True,
+    'use_soundboard': True,
+    'request_to_speak': True
+}
+
+text_channel_permissions = {
+    'embed_links': True
+}
